4131823665a1cdfb421f8af6c1cdf79baff7a923
imhele <work@imhele.com>
2021-08-08 16:13:15 +0800

    chore: 监听函数支持配置选择

--- packages/runtime/src/setup/index.ts
+++ packages/runtime/src/setup/index.ts
@@ -4,25 +4,50 @@ import { setupXMLHttpRequest } from './setupXMLHttpRequest';
 
 let reset: (() => void) | undefined;
 
+export interface SetupNProgressPluginRuntimeOptions {
+  /**
+   * 启用对 XMLHttpRequest 的监听。
+   *
+   * @default
+   * ```ts
+   * true
+   * ```
+   */
+  readonly XMLHttpRequest?: boolean;
+  /**
+   * 启用对 fetch 的监听。
+   *
+   * @default
+   * ```ts
+   * true
+   * ```
+   */
+  readonly fetch?: boolean;
+}
+
 /**
  * 重写 fetch 与 XMLHttpRequest ，以捕获浏览器请求的发起，并在期间展示进度条。
  *
  * @returns 返回一个函数用于撤销重写操作，也可以直接调用 `resetNProgressPluginRuntime()` ，效果是一样的。
  */
-export function setupNProgressPluginRuntime(): () => void {
-  // 已经 setup 过了，无需重复处理
-  if (reset) return reset;
+export function setupNProgressPluginRuntime(
+  options: SetupNProgressPluginRuntimeOptions = {},
+): () => void {
+  // 已经 setup 过了，先重置
+  if (reset) reset();
 
   // 非浏览器环境不启用
   if (typeof window === 'undefined') return () => {};
 
+  const { XMLHttpRequest: listenXMLHttpRequest = true, fetch: listenFetch = true } = options;
+
   const progress = new Progress();
-  const resetFetch = setupFetch(window, progress);
-  const resetXMLHttpRequest = setupXMLHttpRequest(window, progress);
+  const resetFetch = listenFetch ? setupFetch(window, progress) : null;
+  const resetXMLHttpRequest = listenXMLHttpRequest ? setupXMLHttpRequest(window, progress) : null;
 
   reset = () => {
-    resetFetch();
-    resetXMLHttpRequest();
+    if (resetFetch) resetFetch();
+    if (resetXMLHttpRequest) resetXMLHttpRequest();
     reset = undefined;
   };
 
