ab064c05406b7077a60b87c8d336299318d621f0
imhele <work@imhele.com>
2021-08-11 00:30:55 +0800

    fix: 修复临时文件的导出问题

--- packages/plugin/src/constants.ts
+++ packages/plugin/src/constants.ts
@@ -6,7 +6,7 @@ export const RuntimeAPIIe11CjsDirectory = 'ie11-lib';
 
 export const RuntimeAPIIe11EsmDirectory = 'ie11-es';
 
-export const RuntimeInjectionFilePath = `plugin-${PluginKey}/inject.ts`;
+export const RuntimeInjectionFilePath = `./plugin-${PluginKey}/inject.ts`;
 
 export const NProgressPkgName = 'nprogress';
 
