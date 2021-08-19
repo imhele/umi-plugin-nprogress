73264f97cb10648c197228e174adb622ff74d26a
imhele <work@imhele.com>
2021-08-19 22:49:10 +0800

    fix: 修复 worker 环境下，babel ConfigItem 无法被序列化的问题

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+const { createConfigItem } = require('@babel/core');
 const { loadBabelConfig, loadCoreConfig, loadCoreConfigFiles } = require('wc-bundler');
 
 const babel = loadBabelConfig(loadCoreConfig(loadCoreConfigFiles(undefined)).babel, {
@@ -5,9 +6,26 @@ const babel = loadBabelConfig(loadCoreConfig(loadCoreConfigFiles(undefined)).bab
   typescript: true,
 });
 
+makeBabelPluginConfigSerializable(babel.plugins);
+makeBabelPluginConfigSerializable(babel.presets);
+
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
   transform: { [/\.[jt]sx?$/.source]: ['babel-jest', babel] },
 };
 
 module.exports = config;
+
+/**
+ * Jest 启用 worker 执行测试用例时， babel 的 ConfigItem 无法被序列化传入 worker ，
+ * 也无法通过缓存文件共享，因此需要转换一下。
+ *
+ * @param {import('@babel/core').PluginItem[]} plugins
+ */
+function makeBabelPluginConfigSerializable(plugins = []) {
+  plugins.forEach((plugin, index) => {
+    const { file = {}, name = [], options = {}, value } = createConfigItem(plugin);
+    // undefined 会被序列化为 null ，无法通过 babel 校验，因此需要特殊处理一下
+    plugins[index] = [file.resolved || value, options].concat(name);
+  });
+}
