6964dd5a706e50aecf714719582d734bb2a43f39
imhele <work@imhele.com>
2021-08-19 14:59:54 +0800

    perf: 默认启用异步更新 nprogress 状态，以减少对请求的阻塞

--- packages/runtime/src/setup/progress.ts
+++ packages/runtime/src/setup/progress.ts
@@ -12,15 +12,23 @@ export interface RequestHandle {
  */
 export class Progress {
   /**
+   * 异步更新 NProgress 的进度条，减少对请求的阻塞。
+   */
+  public async: boolean;
+
+  /**
    * 请求发起后超过 timeout 则被认为超时。
    *
    * 设置为 0 或是负数时不生效。
    */
   public timeout: number;
 
+  protected checking?: Promise<boolean>;
+
   protected readonly pool = new PendingRequestPool();
 
-  constructor(timeout = 60 * 1000) {
+  constructor(timeout = 60 * 1000, async = true) {
+    this.async = async;
     this.timeout = timeout;
   }
 
@@ -68,9 +76,29 @@ export class Progress {
   }
 
   /**
+   * 请求检查当前是否存在未执行完成的请求记录，并更新 NProgress 状态。
+   *
+   * - 当 async 为 true 时，将在下一个微任务中进行检查。
+   *   如果当前已经存在暂未执行的微任务，不会重复创建。
+   * - 当 async 为 false 时，立即执行检查。
+   */
+  public check(): Promise<boolean> | boolean {
+    if (this.async) {
+      this.checking ||= Promise.resolve().then(() => {
+        this.checking = undefined;
+        return this.checkImmediately();
+      });
+
+      return this.checking;
+    }
+
+    return this.checkImmediately();
+  }
+
+  /**
    * 检查当前是否存在未执行完成的请求记录，并更新 NProgress 状态。
    */
-  public check(): boolean {
+  public checkImmediately(): boolean {
     if (!(this.pool.size > 0)) {
       NProgress.done();
       return false;
