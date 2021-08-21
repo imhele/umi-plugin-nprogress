f140dcc3dcee9666c38e0b01dd2ec3e0f2768b92
imhele <work@imhele.com>
2021-08-22 01:28:14 +0800

    fix: 修复 ci spawn 执行 build 失败的问题

--- scripts/internal/postinstall/index.js
+++ scripts/internal/postinstall/index.js
@@ -1,6 +1,3 @@
 (async () => {
   await require('./yorkie')();
-  const build = require('child_process').spawn('pnpm', ['run', 'build']);
-  build.stdout.pipe(process.stdout, { end: false });
-  build.stderr.pipe(process.stderr, { end: false });
 })();
